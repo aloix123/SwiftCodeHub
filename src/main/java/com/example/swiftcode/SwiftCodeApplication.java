eb05231a8870aa9211ea7b8e173af80803a19242
aloix123 <69531892+aloix123@users.noreply.github.com>
2025-03-31 13:28:56 +0200

    okay there is a progres with docker

--- src/main/java/com/example/swiftcode/SwiftCodeApplication.java
+++ src/main/java/com/example/swiftcode/SwiftCodeApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SwiftCodeApplication {
+public class  SwiftCodeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SwiftCodeApplication.class, args);
